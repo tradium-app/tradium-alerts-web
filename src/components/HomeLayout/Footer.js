aab20145902124319fb880dc463a016b4f1230f8
Suraj Shrestha <syuraj@gmail.com>
2020-08-29 01:57:23 -0400

    name change

--- src/components/HomeLayout/Footer.js
+++ src/components/HomeLayout/Footer.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Container, Row, Col } from 'reactstrap'
 
-const Footer = (props) => {
+const Footer = () => {
     return (
         <React.Fragment>
             <footer className="footer">
                 <Container fluid={true}>
                     <Row>
-                        <Col md={6}>{new Date().getFullYear()} © Skote.</Col>
+                        <Col md={6}>{new Date().getFullYear()} © Webtutor.io</Col>
                         <Col md={6}>
-                            <div className="text-sm-right d-none d-sm-block">Design & Develop by Themesbrand</div>
+                            <div className="text-sm-right d-none d-sm-block">Developed by Webtutor Inc. Indianapolis, Indiana, USA</div>
                         </Col>
                     </Row>
                 </Container>
