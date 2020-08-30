88f5c4c3d8e28a0c1db7b4bc8e612312aa970adb
Suraj Shrestha <syuraj@gmail.com>
2020-08-29 22:00:15 -0400

    ui html change

--- src/pages/Find-Tutors/Find-Tutors.js
+++ src/pages/Find-Tutors/Find-Tutors.js
@@ -1,63 +1,17 @@
 import React, { useState } from 'react'
 
-import { Link } from 'react-router-dom'
-import {
-    Container,
-    Row,
-    Col,
-    Card,
-    CardBody,
-    CardTitle,
-    Form,
-    Label,
-    Input,
-    Nav,
-    NavItem,
-    NavLink,
-    Pagination,
-    PaginationItem,
-    PaginationLink,
-} from 'reactstrap'
-import classnames from 'classnames'
+import { Container, Row, Col, Card, CardBody, CardTitle, Form, Label, Input, Pagination, PaginationItem, PaginationLink } from 'reactstrap'
 
-//Import Star Ratings
 import StarRatings from 'react-star-ratings'
 
 // RangeSlider
 import Nouislider from 'nouislider-react'
 import 'nouislider/distribute/nouislider.css'
 
-//Import Product Images
-import product1 from '../../assets/images/product/img-1.png'
-import product2 from '../../assets/images/product/img-2.png'
-import product3 from '../../assets/images/product/img-3.png'
-import product4 from '../../assets/images/product/img-4.png'
-import product5 from '../../assets/images/product/img-5.png'
-import product6 from '../../assets/images/product/img-6.png'
+import avatar3 from '../../assets/images/users/avatar-3.jpg'
 
 const FindTutors = (props) => {
     const [activeTab, setactiveTab] = useState(false)
-
-    const FilterClothes = [
-        { id: 1, name: 'T-shirts', link: '#' },
-        { id: 2, name: 'Shirts', link: '#' },
-        { id: 3, name: 'Jeans', link: '#' },
-        { id: 4, name: 'Jackets', link: '#' },
-    ]
-    const Products = [
-        { id: 1, image: product1, name: 'Half sleeve T-shirt', link: '#', rating: 5, oldPrice: 500, newPrice: 450, isOffer: true, offer: -25 },
-        { id: 2, image: product2, name: 'Light blue T-shirt', link: '#', rating: 4, oldPrice: 240, newPrice: 225, isOffer: false, offer: 0 },
-        { id: 3, image: product3, name: 'Black Color T-shirt', link: '#', rating: 4, oldPrice: 175, newPrice: 152, isOffer: true, offer: -20 },
-        { id: 4, image: product4, name: 'Hoodie (Blue)', link: '#', rating: 4, oldPrice: 150, newPrice: 145, isOffer: false, offer: 0 },
-        { id: 5, image: product5, name: 'Half sleeve T-Shirt', link: '#', rating: 4, oldPrice: 145, newPrice: 138, isOffer: true, offer: -22 },
-        { id: 6, image: product6, name: 'Green color T-shirt', link: '#', rating: 4, oldPrice: 138, newPrice: 135, isOffer: true, offer: -28 },
-    ]
-
-    function toggleTab(tab) {
-        if (activeTab !== tab) {
-            setactiveTab(tab)
-        }
-    }
 
     return (
         <React.Fragment>
@@ -92,7 +46,7 @@ const FindTutors = (props) => {
                                         <div className="custom-control custom-checkbox mt-2">
                                             <Input type="checkbox" value="0" className="custom-control-input" id="productdiscountCheck1" />
                                             <Label className="custom-control-label" htmlFor="productdiscountCheck1">
-                                                > 500
+                                                &gt; 500
                                             </Label>
                                         </div>
                                         <div className="custom-control custom-checkbox mt-2">
@@ -180,86 +134,46 @@ const FindTutors = (props) => {
 
                         <Col lg="9">
                             <Row className="mb-3">
-                                <Col xl="4" sm="6">
-                                    <div className="mt-2">
-                                        <h5>Clothes</h5>
-                                    </div>
-                                </Col>
-                                <Col lg="8" sm="6">
-                                    <Form className="mt-4 mt-sm-0 float-sm-right form-inline">
-                                        <div className="search-box mr-2">
+                                <Col>
+                                    <Form className="mt-6 form-inline">
+                                        <div className="search-box mr-0">
                                             <div className="position-relative">
                                                 <Input type="text" className="form-control border-0" placeholder="Search..." />
                                                 <i className="bx bx-search-alt search-icon"></i>
                                             </div>
                                         </div>
-                                        <Nav className="product-view-nav" pills>
-                                            <NavItem>
-                                                <NavLink
-                                                    className={classnames({ active: activeTab === '1' })}
-                                                    onClick={() => {
-                                                        toggleTab('1')
-                                                    }}
-                                                >
-                                                    <i className="bx bx-grid-alt"></i>
-                                                </NavLink>
-                                            </NavItem>
-                                            <NavItem>
-                                                <NavLink
-                                                    className={classnames({ active: activeTab === '2' })}
-                                                    onClick={() => {
-                                                        toggleTab('2')
-                                                    }}
-                                                >
-                                                    <i className="bx bx-list-ul"></i>
-                                                </NavLink>
-                                            </NavItem>
-                                        </Nav>
                                     </Form>
                                 </Col>
                             </Row>
+                            <div className="mb-2">
+                                <h7>3 results</h7>
+                            </div>
                             <Row>
-                                {Products.map((product, key) => (
-                                    <Col xl="4" sm="6" key={'_col_' + key}>
-                                        <Card>
-                                            <CardBody>
-                                                <div className="product-img position-relative">
-                                                    {product.isOffer ? (
-                                                        <div className="avatar-sm product-ribbon">
-                                                            <span className="avatar-title rounded-circle  bg-primary">{product.offer + '%'}</span>
-                                                        </div>
-                                                    ) : null}
-
-                                                    <img src={product.image} alt="" className="img-fluid mx-auto d-block" />
+                                <Col className="col-12">
+                                    <Card>
+                                        <CardBody>
+                                            <div className="media mb-4">
+                                                <img className="d-flex align-self-start rounded mr-4" src={avatar3} alt="Skote" height="100" />
+                                                <div className="media-body">
+                                                    <h5 className="mt-0 font-16">Khagendra Shah</h5>
+                                                    <StarRatings
+                                                        rating={4}
+                                                        starRatedColor="#F1B44C"
+                                                        starEmptyColor="#2D363F"
+                                                        numberOfStars={5}
+                                                        name="rating"
+                                                        starDimension="12px"
+                                                        starSpacing="3px"
+                                                    />
+                                                    <p>
+                                                        I'm a full-stack developer with 15+ years of experience in web application development and
+                                                        code mentoring. Are you stuck? Let me help!
+                                                    </p>
                                                 </div>
-                                                <div className="mt-4 text-center">
-                                                    <h5 className="mb-3 text-truncate">
-                                                        <Link to={'/ecommerce-product-detail/' + product.id} className="text-dark">
-                                                            {product.name}{' '}
-                                                        </Link>
-                                                    </h5>
-                                                    <div className="text-muted mb-3">
-                                                        <StarRatings
-                                                            rating={product.rating}
-                                                            starRatedColor="#F1B44C"
-                                                            starEmptyColor="#2D363F"
-                                                            numberOfStars={5}
-                                                            name="rating"
-                                                            starDimension="14px"
-                                                            starSpacing="3px"
-                                                        />
-                                                    </div>
-                                                    <h5 className="my-0">
-                                                        <span className="text-muted mr-2">
-                                                            <del>${product.oldPrice}</del>
-                                                        </span>{' '}
-                                                        <b>${product.newPrice}</b>
-                                                    </h5>
-                                                </div>
-                                            </CardBody>
-                                        </Card>
-                                    </Col>
-                                ))}
+                                            </div>
+                                        </CardBody>
+                                    </Card>
+                                </Col>
                             </Row>
 
                             <Row>
