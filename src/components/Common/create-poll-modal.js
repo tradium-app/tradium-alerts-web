717491f961202b9554cb2ece177593ff8afcaa3c
Suraj Shrestha <syuraj@gmail.com>
2021-05-19 01:22:19 -0400

    cleanup create-poll modal dialog

--- src/components/Common/create-poll-modal.js
+++ src/components/Common/create-poll-modal.js
@@ -1,8 +1,5 @@
 import React from 'react'
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Table } from 'reactstrap'
-
-import modalimage1 from '../../assets/images/product/img-7.png'
-import modalimage2 from '../../assets/images/product/img-4.png'
+import { Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Col, Input, Button } from 'reactstrap'
 
 const CreatePollModal = ({ isShowing, toggle }) => {
     return isShowing ? (
@@ -10,72 +7,23 @@ const CreatePollModal = ({ isShowing, toggle }) => {
             <div className="modal-content">
                 <ModalHeader toggle={toggle}>Create a Poll</ModalHeader>
                 <ModalBody>
-                    <p className="mb-2">
-                        Product id: <span className="text-primary">#SK2540</span>
-                    </p>
-                    <p className="mb-4">
-                        Billing Name: <span className="text-primary">Neal Matthews</span>
-                    </p>
-
-                    <div className="table-responsive">
-                        <Table className="table table-centered table-nowrap">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Product</th>
-                                    <th scope="col">Product Name</th>
-                                    <th scope="col">Price</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">
-                                        <div>
-                                            <img src={modalimage1} alt="" className="avatar-sm" />
-                                        </div>
-                                    </th>
-                                    <td>
-                                        <div>
-                                            <h5 className="text-truncate font-size-14">Wireless Headphone (Black)</h5>
-                                            <p className="text-muted mb-0">$ 225 x 1</p>
-                                        </div>
-                                    </td>
-                                    <td>$ 255</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        <div>
-                                            <img src={modalimage2} alt="" className="avatar-sm" />
-                                        </div>
-                                    </th>
-                                    <td>
-                                        <div>
-                                            <h5 className="text-truncate font-size-14">Hoodie (Blue)</h5>
-                                            <p className="text-muted mb-0">$ 145 x 1</p>
-                                        </div>
-                                    </td>
-                                    <td>$ 145</td>
-                                </tr>
-                                <tr>
-                                    <td colSpan="2">
-                                        <h6 className="m-0 text-right">Sub Total:</h6>
-                                    </td>
-                                    <td>$ 400</td>
-                                </tr>
-                                <tr>
-                                    <td colSpan="2">
-                                        <h6 className="m-0 text-right">Shipping:</h6>
-                                    </td>
-                                    <td>Free</td>
-                                </tr>
-                                <tr>
-                                    <td colSpan="2">
-                                        <h6 className="m-0 text-right">Total:</h6>
-                                    </td>
-                                    <td>$ 400</td>
-                                </tr>
-                            </tbody>
-                        </Table>
-                    </div>
+                    <Form className="justify-content-center">
+                        <FormGroup className="mb-4 justify-content-center" row>
+                            <Col md="12">
+                                <textarea type="text" className="form-control" id="poll-question" placeholder="Start with a Question" rows="3" />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup className="mb-4 justify-content-center" row>
+                            <Col md="10" xs="10">
+                                <Input type="text" className="form-control" id="option1" placeholder="Option 1" />
+                            </Col>
+                            <Col md="2" xs="2">
+                                <button type="button" className="inner btn btn-primary">
+                                    {'+'}
+                                </button>
+                            </Col>
+                        </FormGroup>
+                    </Form>
                 </ModalBody>
                 <ModalFooter>
                     <Button type="button" color="secondary" onClick={toggle}>
