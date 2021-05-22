91d91f7da955d85c8bfb63280f63d73490f495b4
Suraj Shrestha <syuraj@gmail.com>
2021-05-22 03:02:56 -0400

    using firebase analytics library intead of react-ga for now

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import { FirebaseAuthProvider } from '@react-firebase/auth'
 import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
@@ -10,27 +7,15 @@ import { Provider } from 'react-redux'
 import store from './store'
 import { ApolloProvider } from '@apollo/client/react'
 import graphqlClient from './graphql-client'
-
-const firebaseConfig = {
-    apiKey: process.env.REACT_APP_APIKEY,
-    authDomain: process.env.REACT_APP_AUTHDOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASEURL,
-    projectId: process.env.REACT_APP_PROJECTID,
-    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-    appId: process.env.REACT_APP_APPID,
-    measurementId: process.env.REACT_APP_MEASUREMENTID,
-}
+import './firebaseInit'
 
 const app = (
     <ApolloProvider client={graphqlClient}>
-        <FirebaseAuthProvider {...firebaseConfig} firebase={firebase}>
-            <Provider store={store}>
-                <BrowserRouter>
-                    <App />
-                </BrowserRouter>
-            </Provider>
-        </FirebaseAuthProvider>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
     </ApolloProvider>
 )
 
