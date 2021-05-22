e5a0a4fc87d583f27c7cb15021179db13a0c0b8a
Suraj Shrestha <syuraj@gmail.com>
2021-05-22 03:08:42 -0400

    remove some unused libraries

--- src/routes/allRoutes.js
+++ src/routes/allRoutes.js
@@ -1,7 +1,6 @@
 import Home from '../pages/Home'
 
 import PagesMaintenance from '../pages/Utility/pages-maintenance'
-import PagesComingsoon from '../pages/Utility/pages-comingsoon'
 import Pages404 from '../pages/Utility/pages-404'
 import Pages500 from '../pages/Utility/pages-500'
 
@@ -14,7 +13,6 @@ const publicRoutes = [
     { path: '/', component: Home },
     { path: '/profile', component: Profile },
     { path: '/pages-maintenance', component: PagesMaintenance },
-    { path: '/pages-comingsoon', component: PagesComingsoon },
     { path: '/pages-404', component: Pages404 },
     { path: '/pages-500', component: Pages500 },
 ]
