364ab8e5ca8874b6b74e8f0eb685d5ad828ff915
Suraj Shrestha <syuraj@gmail.com>
2021-05-24 21:24:30 -0400

    minor tweaks in Top Trending Polls section

--- src/pages/Home/TopTrendingPolls/Index.js
+++ src/pages/Home/TopTrendingPolls/Index.js
@@ -16,28 +16,28 @@ const TopTrendingPolls = ({ fetchTopPolls, topPolls }) => {
                     <CardTitle className="mb-4">Top Trending Polls</CardTitle>
 
                     <div className="table-responsive">
-                        <Table className="table table-centered table-nowrap">
+                        <Table className="table table-centered">
                             <tbody>
                                 {topPolls &&
                                     topPolls.slice(0, 5).map((poll) => (
                                         <tr key={poll._id}>
                                             <td style={{ width: '50px' }}>
-                                                {poll.author?.imageUrl !== 'Null' ? (
-                                                    <img src={poll.author?.imageUrl} className="rounded-circle avatar-xs" alt="" />
-                                                ) : (
-                                                    <div className="avatar-xs">
-                                                        <span className="avatar-title rounded-circle bg-soft-primary text-primary font-size-16">
-                                                            {poll.author?.name.charAt(0)}
-                                                        </span>
-                                                    </div>
-                                                )}
+                                                <Link to={'/profile/' + poll.author?._id} className="text-dark font-size-14 m-0">
+                                                    {poll.author?.imageUrl !== 'Null' ? (
+                                                        <img src={poll.author?.imageUrl} className="rounded-circle avatar-xs" alt="" />
+                                                    ) : (
+                                                        <div className="avatar-xs">
+                                                            <span className="avatar-title rounded-circle bg-soft-primary text-primary font-size-16">
+                                                                {poll.author?.name.charAt(0)}
+                                                            </span>
+                                                        </div>
+                                                    )}
+                                                </Link>
                                             </td>
                                             <td>
-                                                <h5 className="font-size-14 m-0">
-                                                    <Link to="" className="text-dark">
-                                                        {poll.question}
-                                                    </Link>
-                                                </h5>
+                                                <Link to="" className="text-dark font-size-14 m-0">
+                                                    {poll.question.length > 100 ? poll.question.substring(0, 100) + '...' : poll.question}
+                                                </Link>
                                                 <div>
                                                     {poll.question.topics &&
                                                         poll.question.topics.map((topic, key) => (
