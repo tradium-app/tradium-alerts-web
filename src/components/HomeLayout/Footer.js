9ce0e736c33949fa14ca282da8ed8ab5e8bb082a
Suraj Shrestha <syuraj@gmail.com>
2021-05-26 01:11:23 -0400

    add a link to request a feature

--- src/components/HomeLayout/Footer.js
+++ src/components/HomeLayout/Footer.js
@@ -8,6 +8,16 @@ const Footer = () => {
                 <Container fluid={true}>
                     <Row>
                         <Col md={6}>{new Date().getFullYear()} © DevPolls</Col>
+                        <Col md={6}>
+                            <a
+                                href="https://github.com/devpolls/devpolls-web/issues"
+                                target="_blank"
+                                rel="noreferrer noopener"
+                                className="text-reset"
+                            >
+                                <div className="text-sm-right d-none d-sm-block">Request a feature</div>
+                            </a>
+                        </Col>
                     </Row>
                 </Container>
             </footer>
