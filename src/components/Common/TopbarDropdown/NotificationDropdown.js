67bd63693e98defaaf2b2bd4c1450bcf338ec29f
Suraj Shrestha <syuraj@gmail.com>
2021-05-29 20:03:00 -0400

    fetch notifications from backend

--- src/components/Common/TopbarDropdown/NotificationDropdown.js
+++ src/components/Common/TopbarDropdown/NotificationDropdown.js
@@ -1,116 +1,76 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { useQuery } from '@apollo/client'
+import gql from 'graphql-tag'
 import { Dropdown, DropdownToggle, DropdownMenu, Row, Col } from 'reactstrap'
 import SimpleBar from 'simplebar-react'
+import { getRelativeTime } from '../time'
 
-//Import images
-import avatar3 from '../../../assets/images/users/avatar-3.jpg'
-import avatar4 from '../../../assets/images/users/avatar-4.jpg'
-
-const NotificationDropdown = (props) => {
+const NotificationDropdown = () => {
     const [menu, setMenu] = useState(false)
+    const { loading, error, data } = useQuery(GET_NOTIFICATIONS_QUERY)
 
     return (
-        <>
-            <Dropdown isOpen={menu} toggle={() => setMenu(!menu)} className="dropdown d-inline-block" tag="li">
-                <DropdownToggle className="btn header-item noti-icon waves-effect" tag="button" id="page-header-notifications-dropdown">
-                    <i className="bx bx-bell"></i>
-                    <span className="badge badge-danger badge-pill">3</span>
-                </DropdownToggle>
+        <Dropdown isOpen={menu} toggle={() => setMenu(!menu)} className="dropdown d-inline-block" tag="li">
+            <DropdownToggle className="btn header-item noti-icon waves-effect" tag="button" id="page-header-notifications-dropdown">
+                <i className="bx bx-bell"></i>
+                <span className="badge badge-danger badge-pill">{data?.getNotifications.filter((n) => !n.isRead).length}</span>
+            </DropdownToggle>
 
-                <DropdownMenu className="dropdown-menu dropdown-menu-lg p-0" right>
-                    <div className="p-3">
-                        <Row className="align-items-center">
-                            <Col>
-                                <h6 className="m-0"> {'Notifications'} </h6>
-                            </Col>
-                            <div className="col-auto">
-                                <a href="#!" className="small">
-                                    {'View All'}
-                                </a>
-                            </div>
-                        </Row>
-                    </div>
+            <DropdownMenu className="dropdown-menu dropdown-menu-lg p-0" right>
+                <div className="p-3">
+                    <Row className="align-items-center">
+                        <Col>
+                            <h6 className="m-0"> {'Notifications'} </h6>
+                        </Col>
+                    </Row>
+                </div>
 
-                    <SimpleBar style={{ height: '230px' }}>
-                        <Link to="" className="text-reset notification-item">
-                            <div className="media">
-                                <div className="avatar-xs mr-3">
-                                    <span className="avatar-title bg-primary rounded-circle font-size-16">
-                                        <i className="bx bx-cart"></i>
-                                    </span>
-                                </div>
-                                <div className="media-body">
-                                    <h6 className="mt-0 mb-1">{'Your order is placed'}</h6>
-                                    <div className="font-size-12 text-muted">
-                                        <p className="mb-1">{'If several languages coalesce the grammar'}</p>
-                                        <p className="mb-0">
-                                            <i className="mdi mdi-clock-outline"></i> {'3 min ago'}{' '}
-                                        </p>
+                <SimpleBar style={{ maxHeight: '400px' }}>
+                    {!error &&
+                        !loading &&
+                        data?.getNotifications.map((notification, index) => (
+                            <Link
+                                to={`/${notification.user?.userUrlId}/${notification.poll?.pollUrlId}`}
+                                className="text-reset notification-item"
+                                key={index}
+                                onClick={() => setMenu(false)}
+                            >
+                                <div className="media">
+                                    <img src={notification.user.imageUrl} className="mr-3 rounded-circle avatar-xs" alt="user-pic" />
+                                    <div className="media-body">
+                                        <div className="font-size-12 text-muted">
+                                            <p className="mb-1">{notification.message}</p>
+                                            <p className="mb-0">
+                                                <i className="mdi mdi-clock-outline"></i> {getRelativeTime(notification.modifiedDate)}{' '}
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </Link>
-                        <Link to="" className="text-reset notification-item">
-                            <div className="media">
-                                <img src={avatar3} className="mr-3 rounded-circle avatar-xs" alt="user-pic" />
-                                <div className="media-body">
-                                    <h6 className="mt-0 mb-1">James Lemire</h6>
-                                    <div className="font-size-12 text-muted">
-                                        <p className="mb-1">{'It will seem like simplified English' + '.'}</p>
-                                        <p className="mb-0">
-                                            <i className="mdi mdi-clock-outline"></i>
-                                            {'1 hours ago'}{' '}
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </Link>
-                        <Link to="" className="text-reset notification-item">
-                            <div className="media">
-                                <div className="avatar-xs mr-3">
-                                    <span className="avatar-title bg-success rounded-circle font-size-16">
-                                        <i className="bx bx-badge-check"></i>
-                                    </span>
-                                </div>
-                                <div className="media-body">
-                                    <h6 className="mt-0 mb-1">{'Your item is shipped'}</h6>
-                                    <div className="font-size-12 text-muted">
-                                        <p className="mb-1">{'If several languages coalesce the grammar'}</p>
-                                        <p className="mb-0">
-                                            <i className="mdi mdi-clock-outline"></i> {'3 min ago'}
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </Link>
-
-                        <Link to="" className="text-reset notification-item">
-                            <div className="media">
-                                <img src={avatar4} className="mr-3 rounded-circle avatar-xs" alt="user-pic" />
-                                <div className="media-body">
-                                    <h6 className="mt-0 mb-1">Salena Layfield</h6>
-                                    <div className="font-size-12 text-muted">
-                                        <p className="mb-1">{'As a skeptical Cambridge friend of mine occidental' + '.'}</p>
-                                        <p className="mb-0">
-                                            <i className="mdi mdi-clock-outline"></i>
-                                            {'1 hours ago'}{' '}
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </Link>
-                    </SimpleBar>
-                    <div className="p-2 border-top">
-                        <Link className="btn btn-sm btn-link font-size-14 btn-block text-center" to="#">
-                            {' '}
-                            {'View all'}{' '}
-                        </Link>
-                    </div>
-                </DropdownMenu>
-            </Dropdown>
-        </>
+                            </Link>
+                        ))}
+                </SimpleBar>
+            </DropdownMenu>
+        </Dropdown>
     )
 }
+
+export const GET_NOTIFICATIONS_QUERY = gql`
+    query getNotifications {
+        getNotifications {
+            _id
+            message
+            isRead
+            modifiedDate
+            poll {
+                pollUrlId
+            }
+            user {
+                userUrlId
+                imageUrl
+            }
+        }
+    }
+`
 
 export default NotificationDropdown
