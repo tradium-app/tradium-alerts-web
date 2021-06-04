f8be90e99f6af822d8d5911753ca46713fc7c356
Suraj Shrestha <syuraj@gmail.com>
2021-06-03 23:56:29 -0400

    debounce search for tags

--- src/components/Common/TagSelect.js
+++ src/components/Common/TagSelect.js
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import useDebouncedEffect from 'use-debounced-effect-hook'
 import { useField } from 'formik'
 import CreatableSelect from 'react-select/creatable'
 import { useLazyQuery } from '@apollo/client'
@@ -7,15 +9,18 @@ const TagSelect = ({ label, ...props }) => {
     const [field, meta, helpers] = useField(props)
     // const { touched, error, value } = meta
     const { setValue } = helpers
+    const [searchText, setSearchText] = useState('')
 
     const [getTopTags, { loading, data, refetch, error, called }] = useLazyQuery(GET_TOP_TAGS_QUERY)
+
+    useDebouncedEffect(() => getTopTags({ variables: { searchText: searchText } }), [searchText], 1000)
 
     return (
         <CreatableSelect
             id={props.id}
             name={field.name}
             options={data?.getTopTags.map((t) => ({ label: t.tagId, value: t.tagId }))}
-            onInputChange={(newValue) => getTopTags({ variables: { searchText: newValue } })}
+            onInputChange={(newValue) => setSearchText(newValue)}
             onChange={(option) => {
                 setValue(option.map((o) => o.value))
             }}
