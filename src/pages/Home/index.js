022d6b58f9c9c4d4976abe2072270db33e5d87b0
Suraj Shrestha <syuraj@gmail.com>
2021-07-12 21:58:55 -0400

    dummy columns

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,17 @@
 import React from 'react'
-import {
-    Container,
-    Row,
-    Col,
-    Card,
-    CardBody,
-    Table,
-    Label,
-    Input,
-    Badge,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem,
-} from 'reactstrap'
+import { Link } from 'react-router-dom'
+import { Container, Row, Col, Card, CardBody, Table, Input, Badge } from 'reactstrap'
 
-const EcommerceCustomers = (props) => {
+import img1 from '../../assets/images/poll.png'
+
+const HomePage = (props) => {
+    const handleSubmit = (event) => {
+        // if (searchTerm) {
+        //     props.history.push('/search?q=' + searchTerm)
+        // }
+        event.preventDefault()
+    }
+
     return (
         <div className="page-content">
             <Container fluid>
@@ -26,9 +22,12 @@ const EcommerceCustomers = (props) => {
                                 <Row className="mb-2">
                                     <Col sm="9"></Col>
                                     <Col sm="3">
-                                        <div className="position-relative">
-                                            <input type="text" className="form-control" placeholder="Search Stocks..." />
-                                        </div>
+                                        <form onSubmit={handleSubmit} className="search-box mb-2">
+                                            <div className="position-relative">
+                                                <Input type="text" className="form-control" placeholder="Search Stocks..." />
+                                                <i className="bx bx-search-alt search-icon"></i>
+                                            </div>
+                                        </form>
                                     </Col>
                                 </Row>
 
@@ -36,54 +35,37 @@ const EcommerceCustomers = (props) => {
                                     <Table className="table-centered table-nowrap">
                                         <thead>
                                             <tr>
-                                                <th>#</th>
-                                                <th>Username</th>
-                                                <th>Phone / Email</th>
-                                                <th>Address</th>
-                                                <th>Rating</th>
-                                                <th>Wallet Balance</th>
-                                                <th>Joining Date</th>
-                                                <th>Action</th>
+                                                <th>Symbol</th>
+                                                <th>Price</th>
+                                                <th>Change%</th>
+                                                <th>Market Cap.</th>
+                                                <th>3 Month Low</th>
+                                                <th>3 Month High</th>
+                                                <th>RSI</th>
+                                                <th>MACD</th>
+                                                <th></th>
                                             </tr>
                                         </thead>
                                         <tbody>
                                             <tr>
                                                 <td>
-                                                    <div className="custom-control custom-checkbox">
-                                                        <Input type="checkbox" className="custom-control-input" id="customCheck1" />
-                                                        <Label className="custom-control-label" htmlFor="customCheck1">
-                                                            &nbsp;
-                                                        </Label>
-                                                    </div>
+                                                    <img src={img1} alt="" className="avatar-sm img-thumbnail rounded-circle" />
                                                 </td>
-                                                <td>Stephen Rash</td>
-                                                <td>
-                                                    <p className="mb-1">325-250-1106</p>
-                                                    <p className="mb-0">StephenRash@teleworm.us</p>
-                                                </td>
-
-                                                <td>2470 Grove Street Bethpage, NY 11714</td>
+                                                <td>22.33</td>
+                                                <td>3.44</td>
+                                                <td>117.14</td>
                                                 <td>
                                                     <Badge color="success" className="font-size-12">
                                                         <i className="mdi mdi-star mr-1"></i> 4.2
                                                     </Badge>
                                                 </td>
                                                 <td>$5,412</td>
-                                                <td>07 Oct, 2019</td>
+                                                <td>OverSold</td>
+                                                <td>OverSold</td>
                                                 <td>
-                                                    <UncontrolledDropdown>
-                                                        <DropdownToggle href="#" className="card-drop" tag="i">
-                                                            <i className="mdi mdi-dots-horizontal font-size-18"></i>
-                                                        </DropdownToggle>
-                                                        <DropdownMenu right>
-                                                            <DropdownItem href="#">
-                                                                <i className="mdi mdi-pencil font-size-16 text-success mr-2"></i>Edit
-                                                            </DropdownItem>
-                                                            <DropdownItem href="#">
-                                                                <i className="mdi mdi-trash-can font-size-16 text-danger mr-2"></i>Delete
-                                                            </DropdownItem>
-                                                        </DropdownMenu>
-                                                    </UncontrolledDropdown>
+                                                    <Link to="#" onClick={() => {}} className="action-icon text-danger">
+                                                        <i className="mdi mdi-trash-can font-size-18"></i>
+                                                    </Link>
                                                 </td>
                                             </tr>
                                         </tbody>
@@ -98,4 +80,4 @@ const EcommerceCustomers = (props) => {
     )
 }
 
-export default EcommerceCustomers
+export default HomePage
