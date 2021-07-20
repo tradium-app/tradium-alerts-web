e18447a1673141f8fdbed840492619ad655b6192
Suraj Shrestha <syuraj@gmail.com>
2021-07-19 20:57:28 -0400

    getWatchList from backend

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Container, Row, Col, Card, CardBody, Table, Badge, Media } from 'reactstrap'
+import gql from 'graphql-tag'
 import img1 from '../../assets/images/poll.png'
 import SearchStock from '../../components/Common/SearchStock'
+import { useQuery } from '@apollo/client'
 
 const HomePage = (props) => {
-    const handleSelect = (symbol) => {
-        props.history.push('/symbol/' + symbol.value)
-    }
+    const { loading, error, data } = useQuery(GET_WATCHLIST_QUERY, { pollInterval: 30000 })
 
     return (
         <div className="page-content">
@@ -20,7 +20,10 @@ const HomePage = (props) => {
                                     <Col sm="9"></Col>
                                     <Col sm="3">
                                         <div className="position-relative">
-                                            <SearchStock className="form-control mb-2" handleSelect={handleSelect} />
+                                            <SearchStock
+                                                className="form-control mb-2"
+                                                handleSelect={(symbol) => handleSelect(symbol, props.history)}
+                                            />
                                         </div>
                                     </Col>
                                 </Row>
@@ -40,39 +43,7 @@ const HomePage = (props) => {
                                                 <th></th>
                                             </tr>
                                         </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td>
-                                                    <Media className="mb-4">
-                                                        <div className="mr-3">
-                                                            <img src={img1} alt="" className="avatar-xs img-thumbnail rounded-circle" />
-                                                        </div>
-                                                        <Media body>
-                                                            <h5 className="font-size-13 mb-1">
-                                                                <Link to="/symbol/TSLA">TSLA</Link>
-                                                            </h5>
-                                                            <p className="text-muted mb-1">Tesla</p>
-                                                        </Media>
-                                                    </Media>
-                                                </td>
-                                                <td>22.33</td>
-                                                <td>3.44</td>
-                                                <td>117.14</td>
-                                                <td>
-                                                    <Badge color="success" className="font-size-12">
-                                                        <i className="mdi mdi-star mr-1"></i> 4.2
-                                                    </Badge>
-                                                </td>
-                                                <td>$5,412</td>
-                                                <td>OverSold</td>
-                                                <td>OverSold</td>
-                                                <td>
-                                                    <Link to="#" onClick={() => {}} className="action-icon text-danger">
-                                                        <i className="mdi mdi-trash-can font-size-18"></i>
-                                                    </Link>
-                                                </td>
-                                            </tr>
-                                        </tbody>
+                                        <tbody>{data && !error && !loading && data.getWatchList.map(createWatchListRow)}</tbody>
                                     </Table>
                                 </div>
                             </CardBody>
@@ -84,4 +55,54 @@ const HomePage = (props) => {
     )
 }
 
+const createWatchListRow = (stock, index) => {
+    return (
+        <tr key={index}>
+            <td>
+                <Media>
+                    <div className="mr-3">
+                        <img src={img1} alt="" className="avatar-xs img-thumbnail rounded-circle" />
+                    </div>
+                    <Media body>
+                        <h5 className="font-size-13 mb-1">
+                            <Link to={`/symbol/${stock.symbol.toUpperCase()}`}>{stock.symbol.toUpperCase()}</Link>
+                        </h5>
+                        <p className="text-muted mb-1">Tesla</p>
+                    </Media>
+                </Media>
+            </td>
+            <td>{stock.price}</td>
+            <td>3.44</td>
+            <td>117.14</td>
+            <td>
+                <Badge color="success" className="font-size-12">
+                    <i className="mdi mdi-star mr-1"></i> 4.2
+                </Badge>
+            </td>
+            <td>$5,412</td>
+            <td>OverSold</td>
+            <td>OverSold</td>
+            <td>
+                <Link onClick={() => {}} className="action-icon text-danger" to="#">
+                    <i className="mdi mdi-trash-can font-size-18"></i>
+                </Link>
+            </td>
+        </tr>
+    )
+}
+
+export const GET_WATCHLIST_QUERY = gql`
+    query getWatchList {
+        getWatchList {
+            id
+            symbol
+            company
+            price
+        }
+    }
+`
+
+const handleSelect = (symbol, history) => {
+    history.push('/symbol/' + symbol.value)
+}
 export default HomePage
