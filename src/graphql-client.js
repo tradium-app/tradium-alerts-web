1de5170db601efaa02ce0292d9e6d478ca4e5e4c
Suraj Shrestha <syuraj@gmail.com>
2021-10-02 19:46:39 -0400

    console log graphql errors for now

--- src/graphql-client.js
+++ src/graphql-client.js
@@ -1,8 +1,10 @@
 import { ApolloClient, ApolloLink, createHttpLink, InMemoryCache } from '@apollo/client'
 import DebounceLink from 'apollo-link-debounce'
 import 'firebase/auth'
+import { onError } from '@apollo/client/link/error'
 
 const DEFAULT_DEBOUNCE_TIMEOUT = 500
+
 const debounceLink = new DebounceLink(DEFAULT_DEBOUNCE_TIMEOUT)
 
 const httpLink = createHttpLink({
@@ -10,7 +12,15 @@ const httpLink = createHttpLink({
     credentials: 'include',
 })
 
-const link = ApolloLink.from([debounceLink, httpLink])
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors)
+        graphQLErrors.forEach(({ message, locations, path }) =>
+            console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+        )
+    if (networkError) console.log(`[Network error]: ${networkError}`)
+})
+
+const link = ApolloLink.from([errorLink, debounceLink, httpLink])
 
 const graphqlClient = new ApolloClient({
     link,
