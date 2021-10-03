5b27db3a44761f1c122846a44cac362a51587a28
Suraj Shrestha <syuraj@gmail.com>
2021-10-03 12:13:08 -0400

    adding back 20% above ma configs

--- src/pages/Alert/Components/IndicatorValues.js
+++ src/pages/Alert/Components/IndicatorValues.js
@@ -19,6 +19,7 @@ const IndicatorValues = [
         values: [
             { value: 'above_sma20', valueText: 'Price above SMA20', valueConfig: { length: 20, value: 0, upDirection: true } },
             { value: 'above_10_sma20', valueText: 'Price 10% above SMA20', valueConfig: { length: 20, value: 10, upDirection: true } },
+            { value: 'above_20_sma20', valueText: 'Price 20% above SMA20', valueConfig: { length: 20, value: 20, upDirection: true } },
             {
                 value: 'golden_cross_sma20_sma50',
                 valueText: 'Golden Cross (SMA20 above SMA50)',
@@ -26,6 +27,7 @@ const IndicatorValues = [
             },
             { value: 'below_sma20', valueText: 'Price below SMA20', valueConfig: { length: 20, value: 0, upDirection: false } },
             { value: 'below_10_sma20', valueText: 'Price 10% below SMA20', valueConfig: { length: 20, value: 10, upDirection: false } },
+            { value: 'below_20_sma20', valueText: 'Price 20% below SMA20', valueConfig: { length: 20, value: 20, upDirection: false } },
             {
                 value: 'death_cross_sma20_sma50',
                 valueText: 'Death Cross (SMA20 above SMA50)',
@@ -39,6 +41,7 @@ const IndicatorValues = [
         values: [
             { value: 'above_ema20', valueText: 'Price above EMA20', valueConfig: { length: 20, value: 0, upDirection: true } },
             { value: 'above_10_ema20', valueText: 'Price 10% above EMA20', valueConfig: { length: 20, value: 10, upDirection: true } },
+            { value: 'above_20_ema20', valueText: 'Price 20% above EMA20', valueConfig: { length: 20, value: 20, upDirection: true } },
             {
                 value: 'golden_cross_ema20_ema50',
                 valueText: 'Golden Cross (EMA20 above EMA50)',
@@ -46,6 +49,7 @@ const IndicatorValues = [
             },
             { value: 'below_ema20', valueText: 'Price below EMA20', valueConfig: { length: 20, value: 0, upDirection: false } },
             { value: 'below_10_ema20', valueText: 'Price 10% below EMA20', valueConfig: { length: 20, value: 10, upDirection: false } },
+            { value: 'below_20_ema20', valueText: 'Price 10% below EMA20', valueConfig: { length: 20, value: 20, upDirection: false } },
             {
                 value: 'death_cross_ema20_ema50',
                 valueText: 'Death Cross (EMA20 above EMA50)',
