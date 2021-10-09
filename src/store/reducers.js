da75231bb7a8bb51f265a7f5ed1814c4e2dafbab
Suraj Shrestha <syuraj@gmail.com>
2021-10-08 20:14:24 -0400

    fix netlify build failure

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux'
 import Login from './auth/reducer'
 import Profile from './profile/reducer'
 import Home from './home/reducer'
-import SearchReducer from '../pages/Search/store/reducer'
 
 import { LOGOUT_USER } from './auth/actionTypes'
 
@@ -10,7 +9,6 @@ const appReducer = combineReducers({
     Login,
     Profile,
     Home,
-    SearchReducer,
 })
 
 const initialState = appReducer({}, {})
--- src/store/sagas.js
+++ src/store/sagas.js
@@ -3,8 +3,7 @@ import { all } from 'redux-saga/effects'
 import AuthSaga from './auth/saga'
 import ProfileSaga from './profile/saga'
 import HomeSaga from './home/saga'
-import SearchSaga from '../pages/Search/store/saga'
 
 export default function* rootSaga() {
-    yield all([AuthSaga(), ProfileSaga(), HomeSaga(), SearchSaga()])
+    yield all([AuthSaga(), ProfileSaga(), HomeSaga()])
 }
